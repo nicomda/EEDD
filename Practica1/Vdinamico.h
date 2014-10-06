21f79f1dc45aab3431f3f82222f9955d141f941e
nicomda <nicomda@192.168.1.3>
2014-10-06 17:39:37 +0200

    Comenzada implementación de plantilla de vector dinámico

--- Practica1/Vdinamico.h
+++ Practica1/Vdinamico.h
@@ -8,14 +8,66 @@
 #ifndef VDINAMICO_H
 #define	VDINAMICO_H
 
-class Vdinamico {
+template<class T>class Vdinamico {
+private:
+    int tamal,tamaf;
+    int *v;
 public:
     Vdinamico();
     Vdinamico(const Vdinamico& orig);
     virtual ~Vdinamico();
-private:
-
+    int lee(int pos){ return v[pos]; }
+    void escribe(int pos, int dato){ v[pos] = dato; }
+    void inserta(int dato, unsigned pos);
+    int elimina(unsigned pos);
+    void aumenta(int dato); // Inserción por la derecha
+    int disminuye(); // Eliminar dato por la derecha
+    unsigned tama(){ return tamal; };
+    void ordenar();
+    int busca(int dato); 
+    int busquedaBin(int dato);
 };
 
+template<class T>
+Vdinamico<T>::Vdinamico(){
+    //TODO
+}
+
+template<class T>
+Vdinamico<T>::inserta(int dato, unsigned pos){
+    if(tamal==tamaf){
+    int *vaux;
+    vaux= new int[tamaf=tamaf*2];
+    for (unsigned i=tamal-1;i>pos;i--){
+        v[i+1]=v[i];
+    }
+    }
+    v[pos]=dato;
+}
+
+template<class T>
+Vdinamico<T>::aumenta(int dato){
+    if(tamal==tamaf){
+        int *vaux;
+        vaux= new int[tamaf=tamaf*2];
+        for(int i=0;i<tamal;i++)
+            vaux[i]=v[i];
+        delete []v;
+        v=vaux;
+    }
+    v[tamal++]=dato;
+}
+
+template<class T>
+Vdinamico<T>::disminuye(){
+    if (tamal*3<tamaf){
+        tamaf=tamaf/2;
+        int *vaux = new T[tamaf];
+        for(unsigned i=0;i<tamal;i++){
+            vaux[i]=v[i];
+        };
+       
+    }
+}
 #endif	/* VDINAMICO_H */
 
